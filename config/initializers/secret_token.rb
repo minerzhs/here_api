9a4db80cf6e4b7df9d838ecf3636a1f3cc55c0e6
Haisu Zuo <zuohaisu@gmail.com>
2014-07-26 23:44:12 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HereApi::Application.config.secret_key_base = 'e9258a7c5d237d1a75fb76f8025fd91e1af03cd1f70c714d2e1c824507ea8c3fcf25f201b4d29be1256e8a80cc68016ef49cfc4b3544282ad9369aefa8a77ce9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HereApi::Application.config.session_store :cookie_store, key: '_here_api_session'
